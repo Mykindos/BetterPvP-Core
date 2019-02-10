52d24380aac41934b9085489f69e142caa743bb7
Tom <tomhoogstra@live.com.au>
2019-02-11 06:31:42 +1100

    Tidy up, converting to more lambdas

--- src/main/java/net/betterpvp/core/punish/PunishManager.java
+++ src/main/java/net/betterpvp/core/punish/PunishManager.java
@@ -6,6 +6,7 @@ import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 import java.util.UUID;
+import java.util.stream.Collectors;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
@@ -26,147 +27,138 @@ import net.betterpvp.core.utility.UtilMessage;
 import net.betterpvp.core.utility.UtilPlayer;
 
 public class PunishManager extends BPVPListener<Core> {
-	
-	public PunishManager(Core i){
-		super(i);
-		
-	}
 
-	private static Set<Punish> punishments = new HashSet<>();
-	public static Set<UUID> authed = new HashSet<>();
+    public PunishManager(Core i) {
+        super(i);
 
-	@EventHandler(priority = EventPriority.MONITOR)
-	public void onPlayerLogin(PlayerLoginEvent event) {
-		Player player = event.getPlayer();
+    }
 
-		if (isBanned(player.getUniqueId())) {
-			Punish punish = getPunish(player.getUniqueId(), PunishType.BAN);
-
-			if (System.currentTimeMillis() >= punish.getTime() || player.getUniqueId().toString().equals("e1f5d06b-685b-46a0-b22c-176d6aefffff")) {
-				punishments.remove(punish);
-				return;
-			}
-
-			if (punish.getPunishType() == PunishType.PERM_BAN) {
-				event.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.YELLOW + "You have been banned! \n " + ChatColor.YELLOW
-						+ "Remaining: " + ChatColor.GRAY + "Permanent" + " \n"
-						+ ChatColor.YELLOW + "Reason: " + ChatColor.GRAY + punish.getReason()
-						+ "\n  \n " + ChatColor.AQUA + "Appeal at www.battleau.net/forum");
-				return;
-			}
-
-			event.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.YELLOW + "You have been banned! \n " + ChatColor.YELLOW
-					+ "Remaining: " + ChatColor.GRAY + punish.getRemaining() + " \n"
-					+ ChatColor.YELLOW + "Reason: " + ChatColor.GRAY + punish.getReason()
-					+ "\n  \n " + ChatColor.AQUA + "Appeal at www.battleau.net/forumm");
-
-		}
-	}
-
-	public static void addPunishment(Punish p){
-		punishments.add(p);
-	}
-
-	public static void removePunishment(Punish p){
-		punishments.remove(p);
-	}
+    private static Set<Punish> punishments = new HashSet<>();
 
 
-	public static void handlePunishments(){
-		Iterator<Punish> iterator = punishments.iterator();
-		while(iterator.hasNext()){
-			Punish next = iterator.next();
-			if(next.getTime() - System.currentTimeMillis() <= 0){
-				if(Bukkit.getPlayer(next.getPunished()) != null){
-					Player p = Bukkit.getPlayer(next.getPunished());
-					UtilMessage.message(p, "Punish", "Your punishment has been lifted!");
-
-				}
-				PunishRepository.removePunishment(next);
-				iterator.remove();
-			}
-		}
-	}
-
-	public static boolean isPunished(UUID uuid) {
-		for (Punish punish : punishments) {
-			if (punish.getPunished().equals(uuid)) {
-				return true;
-			}
-		}
-		return false;
-	}
 
 
-	@EventHandler
-	public void onQuitMAH(PlayerQuitEvent e){
-        authed.remove(e.getPlayer().getUniqueId());
-	}
+    public static void addPunishment(Punish p) {
+        punishments.add(p);
+    }
 
-	public static List<Punish> getPunishments(UUID uuid){
-		List<Punish> temp = new ArrayList<>();
-		for(Punish p : punishments) {
-			if(p.getPunished().toString().equalsIgnoreCase(uuid.toString())) {
-				temp.add(p);
-			}
-		}
-		return temp;
-	}
-	
-	public static Punish getPunish(UUID uuid, PunishType type) {
-		for (Punish punish : punishments) {
-			if (punish.getPunished().equals(uuid) && punish.getPunishType().equals(type)) {
-				return punish;
-			}
-		}
-		return null;
-	}
+    public static void removePunishment(Punish p) {
+        punishments.remove(p);
+    }
 
-	public static PunishType getPunishType(UUID uuid) {
-		for (Punish punish : punishments) {
-			if (punish.getPunished().equals(uuid)) {
-				return punish.getPunishType();
-			}
-		}
-		return null;
-	}
+    public static boolean isPunished(UUID uuid) {
 
-	public static boolean isMuted(UUID uuid) {
+        return punishments.stream().filter(p -> p.getPunished().equals(uuid)).findAny().isPresent();
+    }
+
+
+    /**
+     * Get a list of punishments associated with the provided UUID
+     * @param uuid Player UUID
+     * @return List of punishments
+     */
+    public static List<Punish> getPunishments(UUID uuid) {
+
+       return punishments.stream().filter(p -> p.getPunished().toString().equalsIgnoreCase(uuid.toString()))
+                .collect(Collectors.toList());
+
+    }
+
+    /**
+     * Get punishment of a specific type belonging to the provided UUID
+     * @param uuid Player UUID
+     * @param type Type of punishment
+     * @return Punish of matching type and UUID
+     */
+    public static Punish getPunish(UUID uuid, PunishType type) {
+
+        return punishments.stream().filter(p -> p.getPunished().equals(uuid)
+                && p.getPunishType() == type).findAny().orElse(null);
+
+    }
+
+    public static boolean isMuted(UUID uuid) {
         return getPunish(uuid, PunishType.MUTE) != null;
     }
-	
-	public static boolean isPvPLocked(UUID uuid) {
+
+    public static boolean isPvPLocked(UUID uuid) {
         return getPunish(uuid, PunishType.PVPLock) != null;
     }
-	
-	
-	public static boolean isBuildLocked(UUID uuid) {
+
+    public static boolean isBuildLocked(UUID uuid) {
         return getPunish(uuid, PunishType.BuildLock) != null;
     }
-	
-	@EventHandler
-	public void onPVPLockDamage(EntityDamageByEntityEvent e){
-		if(UtilPlayer.getPlayer(e.getDamager()) != null){
 
-			Player p = UtilPlayer.getPlayer(e.getDamager());
-			if(isPvPLocked(p.getUniqueId())){
-				Punish punish = getPunish(p.getUniqueId(), PunishType.PVPLock);
-				UtilMessage.message(p, "PvP Lock", "You have been PvP Locked! " + ChatColor.YELLOW + "Time Remaining: " + ChatColor.GREEN 
-						+ punish.getRemaining());
-				e.setCancelled(true);
-			}
-		}
-	}
 
-	/**
-	 * 
-	 * @param uuid UUID of the player
-	 * @return true if player is banned
-	 */
-	public static boolean isBanned(UUID uuid) {
 
-		return getPunish(uuid, PunishType.BAN) != null;
-	
-	}
+    public static void handlePunishments() {
+        Iterator<Punish> iterator = punishments.iterator();
+        while (iterator.hasNext()) {
+            Punish next = iterator.next();
+            if (next.getTime() - System.currentTimeMillis() <= 0) {
+                if (Bukkit.getPlayer(next.getPunished()) != null) {
+                    Player p = Bukkit.getPlayer(next.getPunished());
+                    UtilMessage.message(p, "Punish", "Your punishment has been lifted!");
+
+                }
+                PunishRepository.removePunishment(next);
+                iterator.remove();
+            }
+        }
+    }
+
+    @EventHandler(priority = EventPriority.MONITOR)
+    public void onPlayerLogin(PlayerLoginEvent event) {
+        Player player = event.getPlayer();
+
+        if (isBanned(player.getUniqueId())) {
+            Punish punish = getPunish(player.getUniqueId(), PunishType.BAN);
+
+            if (System.currentTimeMillis() >= punish.getTime() || player.getUniqueId().toString().equals("e1f5d06b-685b-46a0-b22c-176d6aefffff")) {
+                punishments.remove(punish);
+                return;
+            }
+
+            if (punish.getPunishType() == PunishType.PERM_BAN) {
+                event.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.YELLOW + "You have been banned! \n " + ChatColor.YELLOW
+                        + "Remaining: " + ChatColor.GRAY + "Permanent" + " \n"
+                        + ChatColor.YELLOW + "Reason: " + ChatColor.GRAY + punish.getReason()
+                        + "\n  \n " + ChatColor.AQUA + "Appeal at www.battleau.net/forum");
+                return;
+            }
+
+            event.disallow(PlayerLoginEvent.Result.KICK_BANNED, ChatColor.YELLOW + "You have been banned! \n " + ChatColor.YELLOW
+                    + "Remaining: " + ChatColor.GRAY + punish.getRemaining() + " \n"
+                    + ChatColor.YELLOW + "Reason: " + ChatColor.GRAY + punish.getReason()
+                    + "\n  \n " + ChatColor.AQUA + "Appeal at www.battleau.net/forumm");
+
+        }
+    }
+
+
+
+    @EventHandler
+    public void onPVPLockDamage(EntityDamageByEntityEvent e) {
+        if (UtilPlayer.getPlayer(e.getDamager()) != null) {
+
+            Player p = UtilPlayer.getPlayer(e.getDamager());
+            if (isPvPLocked(p.getUniqueId())) {
+                Punish punish = getPunish(p.getUniqueId(), PunishType.PVPLock);
+                UtilMessage.message(p, "PvP Lock", "You have been PvP Locked! " + ChatColor.YELLOW + "Time Remaining: " + ChatColor.GREEN
+                        + punish.getRemaining());
+                e.setCancelled(true);
+            }
+        }
+    }
+
+    /**
+     * @param uuid UUID of the player
+     * @return true if player is banned
+     */
+    public static boolean isBanned(UUID uuid) {
+
+        return getPunish(uuid, PunishType.BAN) != null;
+
+    }
 
 }
