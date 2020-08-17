05d984181599c2738389d44d747ec651e2cfa852
tomhoogstra <tomhoogstra@live.com.au>
2020-08-17 20:45:30 +1000

    Cosmetic update

--- src/main/java/net/betterpvp/core/settings/SettingChangedEvent.java
+++ src/main/java/net/betterpvp/core/settings/SettingChangedEvent.java
@@ -1,5 +1,6 @@
 package net.betterpvp.core.settings;
 
+import net.betterpvp.core.client.Client;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
@@ -8,14 +9,20 @@ public class SettingChangedEvent extends Event {
 
     private static final HandlerList handlers = new HandlerList();
 
+    private Client client;
     private Player player;
+    private String setting;
+    private boolean value;
 
     public static HandlerList getHandlerList() {
         return handlers;
     }
 
-    public SettingChangedEvent(Player player) {
+    public SettingChangedEvent(Client client, Player player, String setting, boolean value) {
+        this.client = client;
         this.player = player;
+        this.setting = setting;
+        this.value = value;
     }
 
     public HandlerList getHandlers() {
@@ -27,4 +34,15 @@ public class SettingChangedEvent extends Event {
     }
 
 
+    public Client getClient() {
+        return client;
+    }
+
+    public String getSetting() {
+        return setting;
+    }
+
+    public boolean getValue() {
+        return value;
+    }
 }
--- src/main/java/net/betterpvp/core/settings/SettingsListener.java
+++ src/main/java/net/betterpvp/core/settings/SettingsListener.java
@@ -13,8 +13,11 @@ import net.betterpvp.core.settings.menu.buttons.SettingsButton;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Sound;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.inventory.ClickType;
+
+import java.util.Map;
 
 public class SettingsListener extends BPVPListener<Core> {
 
@@ -38,18 +41,28 @@ public class SettingsListener extends BPVPListener<Core> {
                     if (e.getClickType() == ClickType.LEFT) {
                         if (!client.getSettingAsBoolean(setting)) {
                             if (client.getSettings().get(setting) != 1) {
+
+                                if (setting.contains("Cosmetic")) {
+                                    disableCosmetics(client, e.getPlayer());
+                                }
+
                                 client.getSettings().put(setting, 1);
                                 SettingsRepository.updateSetting(client.getUUID(), setting, 1);
                                 e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.BLOCK_NOTE_BLOCK_PLING, 1f, 2f);
-                                Bukkit.getPluginManager().callEvent(new SettingChangedEvent(e.getPlayer()));
+                                Bukkit.getPluginManager().callEvent(new SettingChangedEvent(client, e.getPlayer(), setting, true));
                             }
                         }
                     } else if (e.getClickType() == ClickType.RIGHT) {
                         if (client.getSettings().get(setting) != 0) {
+
+                            if (setting.contains("Cosmetic")) {
+                                disableCosmetics(client, e.getPlayer());
+                            }
+
                             client.getSettings().put(setting, 0);
                             SettingsRepository.updateSetting(client.getUUID(), setting, 0);
                             e.getPlayer().playSound(e.getPlayer().getLocation(), Sound.ENTITY_ITEM_BREAK, 1f, 0.6f);
-                            Bukkit.getPluginManager().callEvent(new SettingChangedEvent(e.getPlayer()));
+                            Bukkit.getPluginManager().callEvent(new SettingChangedEvent(client, e.getPlayer(), setting, false));
                         }
                     }
 
@@ -59,6 +72,18 @@ public class SettingsListener extends BPVPListener<Core> {
                 subMenu.buildPage(e.getPlayer());
                 subMenu.construct();
             }
+        }
+    }
+
+    private void disableCosmetics(Client client, Player player) {
+
+        for (Map.Entry<String, Integer> entry : client.getSettings().entrySet()) {
+            if (entry.getKey().contains("Cosmetic")) {
+                entry.setValue(0);
+                Bukkit.getPluginManager().callEvent(new SettingChangedEvent(client, player, entry.getKey(), false));
+                SettingsRepository.updateSetting(client.getUUID(), entry.getKey(), 0);
+            }
+
         }
     }
 }
